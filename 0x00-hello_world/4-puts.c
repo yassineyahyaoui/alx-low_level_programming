a6760e9f98f4d83ff61e2aebfeb60b58c1ce5774
Mohamed Yassine Yahyaoui <55397243+yassineyahyaoui@users.noreply.github.com>
2022-10-13 19:56:42 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/* 
-main prints "Programming is like building a multilingual puzzle"
-*/
+/**
+ * main prints "Programming is like building a multilingual puzzle"
+ */
 
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
