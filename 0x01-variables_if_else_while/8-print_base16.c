592b3ee5deab6ed7f08f6981099530009a935d50
BoutheinaOuerhani <boutheinaouerhani55@gmail.com>
2022-10-14 19:22:59 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+char base_16;
+
+for (base_16 = '0'; base_16 <= '15'; base_16++)
+{
+putchar(base_16);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
