2a755e670c1b383d93dd2d0b53df1f7ce257826f
BoutheinaOuerhani <boutheinaouerhani55@gmail.com>
2022-10-14 18:51:57 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,24 +9,25 @@
 
 int main(void)
 {
-    int n;
+int n;
 
-    n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
-    if (n == 0)
-    {
-        printf("%i is zero\n", n);
-    }
+if (n == 0)
+{
+printf("%i is zero\n", n);
+}
 
-    else if (n < 0)
-    {
-        printf("%i is negative\n", n);
-    }
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
 
-    else
-    {
-        printf("%i is positive\n", n);
-    }
+else
+{
+printf("%i is positive\n", n);
+}
 
-    return (0);
+return (0);
+
 }
