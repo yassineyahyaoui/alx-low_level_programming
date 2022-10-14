57831f6cd996ecccb8e881f8cdb2da98dfdcd704
BoutheinaOuerhani <boutheinaouerhani55@gmail.com>
2022-10-14 19:14:13 +0100

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success/correct)
+ * Return: Always 0 (success/corect)
  */
 int main(void)
 
 {
 int i;
 
-for (i = 0 ; i < 10; i++)
+for (i = 48 ; i <= 57; i++)
 {
-putchar(i);
+putchar (i);
 }
 
-putchar("\n");
+putchar ('\n');
 
 return (0);
 
