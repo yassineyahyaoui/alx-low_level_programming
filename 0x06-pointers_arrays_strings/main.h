017f5d68d17dc6689cf2dfd26f7226a00e9b8d21
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-10-28 02:06:11 +0100

    more pointers tasks

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *rot13(char *);
+void print_buffer(char *b, int size);
+void print_number(int n);
